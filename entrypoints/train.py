6f24d2ebcd2dbfb8897304a4708a5daa6bc25def
fatma.moalla <fatma.moalla@octo.com>
2020-09-30 16:47:39 +0200

    added entrypoint to training

---
+++ entrypoints/train.py
@@ -0,0 +1,37 @@
+import click
+
+from training import main
+
+
+@click.command()
+@click.option('--finetune', default=True, type=bool, help='Finetuning the results')
+@click.option('--feature-model', default='', type=str, help='Path of the pre-trained feature model if it exists')
+@click.option('--relation-model', default='', type=str, help='Path of the pre-trained relation model if it exists')
+@click.option('--learning-rate', '-lr', default=0.001, type=float, help='Learning rate of the optimiser')
+@click.option('--start-episode', '-start', default=0, type=int, help='Start episode when training')
+@click.option('--nbr-episode', '-episode', default=10, type=int, help='Number of episodes when training')
+@click.option('--class-num', '-N', type=int, required=True, help='Number of classes to train, i.e N-way')
+@click.option('--sample-num-per_class', '-K', type=int, required=True,
+              help='Number of images per class to train , i.e K-shot')
+@click.option('--batch-num-per_class', '-batch', type=int, default=3, help='Number of batches per image')
+@click.option('--train-result-path', type=str, default='results_predicted_images_masks',
+              help='Path of the results after training')
+@click.option('--model-save-path', type=str, default='relation_network_trained',
+              help='Path of the relation network after training')
+@click.option('--result-save-freq', type=int, default=10, help='frequency of saving the results')
+@click.option('--model-save-freq', type=int, default=10, help='frequency of saving the model')
+@click.option('--display-query', type=int, default=5, help='Number of test displayed')
+@click.option('--gpu', type=int, default=0, help='GPU to use')
+def entry_point(finetune: bool, feature_model: str, relation_model: str, learning_rate: int,
+                start_episode: int, nbr_episode: int, class_num: int, sample_num_per_class: int,
+                batch_num_per_class: int, train_result_path: str, model_save_path: str,
+                result_save_freq: int, display_query: int, model_save_freq: int, gpu: int):
+    main(finetune, feature_model, relation_model, learning_rate,
+         start_episode, nbr_episode, class_num,
+         sample_num_per_class,
+         batch_num_per_class, train_result_path, model_save_path,
+         result_save_freq, display_query, model_save_freq, gpu)
+
+
+if __name__ == '__main__':
+    entry_point()
--- training.py
+++ training.py
@@ -1,4 +1,4 @@
-import argparse
+import math
 import math
 import os
 import random
@@ -13,47 +13,6 @@ from torch.optim.lr_scheduler import StepLR
 from data_preprocessing.batch_generator import get_training_batch
 from display_data import decode_segmap
 from network import RelationNetwork, CNNEncoder
-
-parser = argparse.ArgumentParser(description="One Shot Visual Recognition")
-parser.add_argument("-f", "--feature_dim", type=int, default=64)
-parser.add_argument("-r", "--relation_dim", type=int, default=8)
-parser.add_argument("-w", "--class_num", type=int, default=1)
-parser.add_argument("-s", "--sample_num_per_class", type=int, default=5)
-parser.add_argument("-b", "--batch_num_per_class", type=int, default=5)
-parser.add_argument("-e", "--episode", type=int, default=800000)
-parser.add_argument("-start", "--start_episode", type=int, default=0)
-parser.add_argument("-t", "--test_episode", type=int, default=1000)
-parser.add_argument("-l", "--learning_rate", type=float, default=0.001)
-parser.add_argument("-g", "--gpu", type=int, default=0)
-parser.add_argument("-u", "--hidden_unit", type=int, default=10)
-parser.add_argument("-d", "--display_query_num", type=int, default=5)
-parser.add_argument("-ex", "--exclude_class", type=int, default=6)
-parser.add_argument("-modelf", "--feature_encoder_model", type=str, default='')
-parser.add_argument("-modelr", "--relation_network_model", type=str, default='')
-parser.add_argument("-lo", "--loadImagenet", type=bool, default=False)
-parser.add_argument("-fi", "--finetune", type=bool, default=True)
-parser.add_argument("-rf", "--TrainResultPath", type=str, default='result_newvgg_1shot')
-parser.add_argument("-rff", "--ResultSaveFreq", type=int, default=10000)
-parser.add_argument("-msp", "--ModelSavePath", type=str, default='models_newvgg_1shot')
-parser.add_argument("-msf", "--ModelSaveFreq", type=int, default=10000)
-
-args = parser.parse_args()
-
-# Hyper Parameters
-FEATURE_DIM = args.feature_dim
-RELATION_DIM = args.relation_dim
-CLASS_NUM = args.class_num
-SAMPLE_NUM_PER_CLASS = args.sample_num_per_class
-BATCH_NUM_PER_CLASS = args.batch_num_per_class
-EPISODE = args.episode
-TEST_EPISODE = args.test_episode
-LEARNING_RATE = args.learning_rate
-GPU = args.gpu
-HIDDEN_UNIT = args.hidden_unit
-DISPLAY_QUERY = args.display_query_num
-EXCLUDE_CLASS = args.exclude_class
-FEATURE_MODEL = args.feature_encoder_model
-RELATION_MODEL = args.relation_network_model
 
 
 def weights_init(m):
@@ -72,7 +31,10 @@ def weights_init(m):
         m.bias.data = torch.ones(m.bias.data.size())
 
 
-def main():
+def main(finetune: bool, feature_model: str, relation_model: str, learning_rate: int,
+         start_episode: int, nbr_episode: int, class_num: int, sample_num_per_class: int,
+         batch_num_per_class: int, train_result_path: str, model_save_path: str,
+         result_save_freq: int, display_query: int, model_save_freq: int, gpu: int):
     # Step 1: init neural networks
     print("init neural networks")
 
@@ -81,57 +43,57 @@ def main():
 
     relation_network.apply(weights_init)
 
-    feature_encoder.cuda(GPU)
-    relation_network.cuda(GPU)
+    feature_encoder.cuda(gpu)
+    relation_network.cuda(gpu)
 
     # fine-tuning
-    if (args.finetune):
-        if os.path.exists(FEATURE_MODEL):
-            feature_encoder.load_state_dict(torch.load(FEATURE_MODEL))
+    if (finetune):
+        if os.path.exists(feature_model):
+            feature_encoder.load_state_dict(torch.load(feature_model))
             print("load feature encoder success")
         else:
-            print('Can not load feature encoder: %s' % FEATURE_MODEL)
+            print('Can not load feature encoder: %s' % feature_model)
             print('starting from scratch')
-        if os.path.exists(RELATION_MODEL):
-            relation_network.load_state_dict(torch.load(RELATION_MODEL))
+        if os.path.exists(relation_model):
+            relation_network.load_state_dict(torch.load(relation_model))
             print("load relation network success")
         else:
-            print('Can not load relation network: %s' % RELATION_MODEL)
+            print('Can not load relation network: %s' % relation_model)
             print('starting from scratch')
 
-    feature_encoder_optim = torch.optim.Adam(feature_encoder.parameters(), lr=LEARNING_RATE)
-    feature_encoder_scheduler = StepLR(feature_encoder_optim, step_size=EPISODE // 10, gamma=0.5)
-    relation_network_optim = torch.optim.Adam(relation_network.parameters(), lr=LEARNING_RATE)
-    relation_network_scheduler = StepLR(relation_network_optim, step_size=EPISODE // 10, gamma=0.5)
+    feature_encoder_optim = torch.optim.Adam(feature_encoder.parameters(), lr=learning_rate)
+    feature_encoder_scheduler = StepLR(feature_encoder_optim, step_size=nbr_episode // 10, gamma=0.5)
+    relation_network_optim = torch.optim.Adam(relation_network.parameters(), lr=learning_rate)
+    relation_network_scheduler = StepLR(relation_network_optim, step_size=nbr_episode // 10, gamma=0.5)
 
     print("Training...")
 
     last_accuracy = 0.0
 
-    for episode in range(args.start_episode, EPISODE):
+    for episode in range(start_episode, nbr_episode):
         feature_encoder_scheduler.step(episode)
         relation_network_scheduler.step(episode)
 
-        samples, sample_labels, batches, batch_labels, chosen_classes = get_training_batch(CLASS_NUM,
-                                                                                           SAMPLE_NUM_PER_CLASS,
-                                                                                           BATCH_NUM_PER_CLASS)
+        samples, sample_labels, batches, batch_labels, chosen_classes = get_training_batch(class_num,
+                                                                                           sample_num_per_class,
+                                                                                           batch_num_per_class)
 
         # calculate features
-        sample_features, _ = feature_encoder(Variable(samples).cuda(GPU))
-        sample_features = sample_features.view(CLASS_NUM, SAMPLE_NUM_PER_CLASS, 512, 7, 7)
+        sample_features, _ = feature_encoder(Variable(samples).cuda(gpu))
+        sample_features = sample_features.view(class_num, sample_num_per_class, 512, 7, 7)
         sample_features = torch.sum(sample_features, 1).squeeze(1)  # 1*512*7*7
-        batch_features, ft_list = feature_encoder(Variable(batches).cuda(GPU))
+        batch_features, ft_list = feature_encoder(Variable(batches).cuda(gpu))
 
         # calculate relations
-        sample_features_ext = sample_features.unsqueeze(0).repeat(BATCH_NUM_PER_CLASS * CLASS_NUM, 1, 1, 1, 1)
-        batch_features_ext = batch_features.unsqueeze(0).repeat(CLASS_NUM, 1, 1, 1, 1)
+        sample_features_ext = sample_features.unsqueeze(0).repeat(batch_num_per_class * class_num, 1, 1, 1, 1)
+        batch_features_ext = batch_features.unsqueeze(0).repeat(class_num, 1, 1, 1, 1)
         batch_features_ext = torch.transpose(batch_features_ext, 0, 1)
 
         relation_pairs = torch.cat((sample_features_ext, batch_features_ext), 2).view(-1, 1024, 7, 7)
-        output = relation_network(relation_pairs, ft_list).view(-1, CLASS_NUM, 224, 224)
+        output = relation_network(relation_pairs, ft_list).view(-1, class_num, 224, 224)
 
-        mse = nn.MSELoss().cuda(GPU)
-        loss = mse(output, Variable(batch_labels).cuda(GPU))
+        mse = nn.MSELoss().cuda(gpu)
+        loss = mse(output, Variable(batch_labels).cuda(gpu))
 
         # training
 
@@ -149,19 +111,19 @@ def main():
         if (episode + 1) % 10 == 0:
             print("episode:", episode + 1, "loss", loss.cpu().data.numpy())
 
-        if not os.path.exists(args.TrainResultPath):
-            os.makedirs(args.TrainResultPath)
-        if not os.path.exists(args.ModelSavePath):
-            os.makedirs(args.ModelSavePath)
+        if not os.path.exists(train_result_path):
+            os.makedirs(train_result_path)
+        if not os.path.exists(model_save_path):
+            os.makedirs(model_save_path)
 
         # training result visualization
-        if (episode + 1) % args.ResultSaveFreq == 0:
-            support_output = np.zeros((224 * 2, 224 * SAMPLE_NUM_PER_CLASS, 3), dtype=np.uint8)
-            query_output = np.zeros((224 * 3, 224 * DISPLAY_QUERY, 3), dtype=np.uint8)
-            chosen_query = random.sample(list(range(0, BATCH_NUM_PER_CLASS)), DISPLAY_QUERY)
+        if (episode + 1) % result_save_freq == 0:
+            support_output = np.zeros((224 * 2, 224 * sample_num_per_class, 3), dtype=np.uint8)
+            query_output = np.zeros((224 * 3, 224 * display_query, 3), dtype=np.uint8)
+            chosen_query = random.sample(list(range(0, batch_num_per_class)), display_query)
 
-            for i in range(CLASS_NUM):
-                for j in range(SAMPLE_NUM_PER_CLASS):
+            for i in range(class_num):
+                for j in range(sample_num_per_class):
                     supp_img = (np.transpose(samples.numpy()[j], (1, 2, 0)) * 255).astype(np.uint8)[:, :, :3][:, :,
                                ::-1]
                     support_output[0:224, j * 224:(j + 1) * 224, :] = supp_img
@@ -187,7 +149,7 @@ def main():
                     result[:, :, 2] = query_pred
                     query_output[224 * 2:224 * 3, cnt * 224:(cnt + 1) * 224, :] = result
             extra = query_output.copy()
-            for i in range(CLASS_NUM):
+            for i in range(class_num):
                 for cnt, x in enumerate(chosen_query):
                     extra_label = batch_labels.numpy()[x][0]
                     extra_label[extra_label != 0] = 255
@@ -196,20 +158,16 @@ def main():
                     result1[:, :, 1] = extra_label
                     result1[:, :, 2] = extra_label
                     extra[224 * 2:224 * 3, cnt * 224:(cnt + 1) * 224, :] = result1
-            cv2.imwrite('%s/%s_query.png' % (args.TrainResultPath, episode), query_output)
-            cv2.imwrite('%s/%s_show.png' % (args.TrainResultPath, episode), extra)
-            cv2.imwrite('%s/%s_support.png' % (args.TrainResultPath, episode), support_output)
+            cv2.imwrite('%s/%s_query.png' % (train_result_path, episode), query_output)
+            cv2.imwrite('%s/%s_show.png' % (train_result_path, episode), extra)
+            cv2.imwrite('%s/%s_support.png' % (train_result_path, episode), support_output)
 
         # save models
-        if (episode + 1) % args.ModelSaveFreq == 0:
+        if (episode + 1) % model_save_freq == 0:
             torch.save(feature_encoder.state_dict(), str(
-                "./%s/feature_encoder_" % args.ModelSavePath + str(episode) + '_' + str(CLASS_NUM) + "_way_" + str(
-                    SAMPLE_NUM_PER_CLASS) + "shot.pkl"))
+                "./%s/feature_encoder_" % model_save_path + str(episode) + '_' + str(class_num) + "_way_" + str(
+                    sample_num_per_class) + "shot.pkl"))
             torch.save(relation_network.state_dict(), str(
-                "./%s/relation_network_" % args.ModelSavePath + str(episode) + '_' + str(CLASS_NUM) + "_way_" + str(
-                    SAMPLE_NUM_PER_CLASS) + "shot.pkl"))
+                "./%s/relation_network_" % model_save_path + str(episode) + '_' + str(class_num) + "_way_" + str(
+                    sample_num_per_class) + "shot.pkl"))
             print("save networks for episode:", episode)
-
-
-if __name__ == '__main__':
-    main()
